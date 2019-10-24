c87616c4dedac3bbe795d3b5b60f1e1820445b6e
William Roush <william.roush@roushtech.net>
2019-10-24 03:18:24 -0400

    Load 800 images

--- WhyNotEarth.Meredith.App/Controllers/Api/v0/PlanetCollageController.cs
+++ WhyNotEarth.Meredith.App/Controllers/Api/v0/PlanetCollageController.cs
@@ -1,7 +1,10 @@
 namespace WhyNotEarth.Meredith.App.Controllers.Api.v0
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using CloudinaryDotNet;
+    using CloudinaryDotNet.Actions;
     using Microsoft.AspNetCore.Cors;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Options;
@@ -19,17 +22,39 @@ namespace WhyNotEarth.Meredith.App.Controllers.Api.v0
             CloudinaryOptions = cloudinaryOptions.Value;
         }
 
+        protected List<SearchResource> GetResources(int totalCount)
+        {
+            var cloudinary = new Cloudinary(new Account(CloudinaryOptions.CloudName, CloudinaryOptions.ApiKey, CloudinaryOptions.ApiSecret));
+            var resources = new List<SearchResource>();
+            string nextCursor = null;
+            do
+            {
+                var requestSize = Math.Min(totalCount, 500);
+                var results = cloudinary.Search()
+                    .Expression("folder=Bensley Website/Monday - Disruption/*")
+                    .MaxResults(requestSize)
+                    .NextCursor(nextCursor)
+                    .Execute();
+                resources.AddRange(results.Resources);
+                totalCount -= 500;
+                nextCursor = results.NextCursor;
+                if (results.Resources.Count == 0 || results.Resources.Count != requestSize)
+                {
+                    break;
+                }
+            }
+            while (totalCount > 0);
+            return resources;
+        }
+
         [HttpGet]
         [Route("")]
         public IActionResult Get()
         {
-            var cloudinary = new Cloudinary(new Account(CloudinaryOptions.CloudName, CloudinaryOptions.ApiKey, CloudinaryOptions.ApiSecret));
-            var result = cloudinary.Search()
-                .Expression("folder=Bensley Website/Monday - Disruption/*")
-                .Execute();
+            var resources = GetResources(800);
             var clodinaryUrl = new Url(CloudinaryOptions.CloudName);
             var imageSize = 75;
-            return Ok(result.Resources.Select(r => new
+            return Ok(resources.Select(r => new
             {
                 Id = r.PublicId,
                 Url = clodinaryUrl
