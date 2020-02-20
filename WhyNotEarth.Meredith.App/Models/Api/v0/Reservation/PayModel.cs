c2050d87c3440e76c50d3ff866b5e62b3671b23c
Shahriar Gholami <shrgholami@gmail.com>
2020-02-20 10:30:33 +0330

    Move reservation total amount from clientside to API

--- WhyNotEarth.Meredith.App/Models/Api/v0/Reservation/PayModel.cs
+++ WhyNotEarth.Meredith.App/Models/Api/v0/Reservation/PayModel.cs
@@ -4,8 +4,6 @@ namespace WhyNotEarth.Meredith.App.Models.Api.v0.Reservation
 {
     public class PayModel
     {
-        public decimal Amount { get; set; }
-
         public Dictionary<string, string> Metadata { get; set; }
     }
 }
