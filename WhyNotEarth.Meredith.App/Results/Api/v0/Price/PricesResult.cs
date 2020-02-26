4e74babf6253898bd1a7aaeea5ce073c5699381d
Shahriar Gholami <shrgholami@gmail.com>
2020-02-26 16:27:06 +0330

    Fix a bug with returning price for room types with no available rooms
    Fixes #72

---
+++ WhyNotEarth.Meredith.App/Results/Api/v0/Price/PricesResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WhyNotEarth.Meredith.App.Results.Api.v0.Price
+{
+    public class PricesResult
+    {
+        public int Id { get; }
+
+        public int RoomTypeId { get; }
+
+        public DateTime Date { get; }
+
+        public decimal Amount { get; }
+
+        public PricesResult(int id, int roomTypeId, DateTime date, decimal amount)
+        {
+            Id = id;
+            RoomTypeId = roomTypeId;
+            Date = date;
+            Amount = amount;
+        }
+    }
+}
