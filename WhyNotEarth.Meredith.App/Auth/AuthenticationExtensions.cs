92c7cee1f3d22abe745d7b60b5f3fb7eaef7a2b2
Shahriar Gholami <shrgholami@gmail.com>
2020-03-16 11:25:06 +0330

    Cleanup the authentication and authorization stuff

--- WhyNotEarth.Meredith.App/Auth/AuthenticationExtensions.cs
+++ WhyNotEarth.Meredith.App/Auth/AuthenticationExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +17,7 @@ using WhyNotEarth.Meredith.Data.Entity;
 using WhyNotEarth.Meredith.Data.Entity.Models;
 using WhyNotEarth.Meredith.Identity;
 
-namespace WhyNotEarth.Meredith.App.ConfigureServices
+namespace WhyNotEarth.Meredith.App.Auth
 {
     public static class AuthenticationExtensions
     {
@@ -26,12 +29,18 @@ namespace WhyNotEarth.Meredith.App.ConfigureServices
             services
                 .AddIdentity<User, Role>()
                 .AddUserManager<UserManager>()
+                .AddRoleManager<RoleManager>()
                 .AddEntityFrameworkStores<MeredithDbContext>()
                 .AddDefaultTokenProviders();
 
             services
-                .AddAuthentication()
-                .AddJwtBearer("jwt", config =>
+                .AddAuthentication(o =>
+                {
+                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    o.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddJwtBearer(config =>
                 {
                     config.RequireHttpsMetadata = false;
                     config.SaveToken = true;
@@ -56,23 +65,38 @@ namespace WhyNotEarth.Meredith.App.ConfigureServices
                     options.ClientId = config["ClientId"];
                     options.ClientSecret = config["ClientSecret"];
                     options.Events.OnRemoteFailure = HandleOnRemoteFailure;
-                })
-                .Services
-                .ConfigureApplicationCookie(options =>
+                });
+
+                services.ConfigureApplicationCookie(options =>
                 {
                     options.Cookie.Name = "auth";
                     options.Cookie.HttpOnly = false;
                     options.Cookie.SameSite = SameSiteMode.None;
                     options.LoginPath = null;
-                    options.Events = new CookieAuthenticationEvents
-                    {
-                        OnRedirectToLogin = redirectContext =>
-                        {
-                            redirectContext.HttpContext.Response.StatusCode = 401;
-                            return Task.CompletedTask;
-                        }
-                    };
                 });
+        }
+
+        public static IApplicationBuilder UseCustomAuthentication(this IApplicationBuilder app)
+        {
+            app.UseAuthentication().
+                Use(async (context, next) =>
+            {
+                // If the default identity failed to authenticate (cookies)
+                if (context.User.Identities.All(i => !i.IsAuthenticated))
+                {
+                    var principal = new ClaimsPrincipal();
+                    var jwtAuth = await context.AuthenticateAsync(JwtBearerDefaults.AuthenticationScheme);
+                    if (jwtAuth?.Principal != null)
+                    {
+                        principal.AddIdentities(jwtAuth.Principal.Identities);
+                        context.User = principal;
+                    }
+                }
+
+                await next();
+            });
+
+            return app;
         }
 
         private static Task HandleOnRemoteFailure(RemoteFailureContext context)
--- WhyNotEarth.Meredith.Data.Entity/Models/Role.cs
+++ WhyNotEarth.Meredith.Data.Entity/Models/Role.cs
@@ -1,10 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace WhyNotEarth.Meredith.Data.Entity.Models
 {
-    using System;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
     public class Role : IdentityRole<int>, IEntityTypeConfiguration<Role>
     {
         public void Configure(EntityTypeBuilder<Role> builder)
