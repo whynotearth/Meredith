48a3f48674dd26ca98fe52966c8d8047473722fa
Shahriar Gholami <shrgholami@gmail.com>
2020-03-16 11:25:06 +0330

    Cleanup the authentication and authorization stuff

---
+++ WhyNotEarth.Meredith.App/Auth/AuthorizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WhyNotEarth.Meredith.App.Auth
+{
+    public static class AuthorizationExtensions
+    {
+        public static void AddCustomAuthorization(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+                options.AddPolicy(Policies.CreatePage, policy => policy.RequireRole(Roles.Admin)));
+        }
+    }
+}
