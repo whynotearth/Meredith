cc9294dd477cacd26c56a9301e295c1f9c703b99
Shahriar Gholami <shrgholami@gmail.com>
2020-04-10 16:57:51 +0430

    Make price and eventDate properties of the post nullable

---
+++ WhyNotEarth.Meredith.Data.Entity/Migrations/20200413091842_RelaxPostProperties.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhyNotEarth.Meredith.Data.Entity.Migrations
+{
+    public partial class RelaxPostProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                schema: "ModuleVolkswagen",
+                table: "Posts",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EventDate",
+                schema: "ModuleVolkswagen",
+                table: "Posts",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                schema: "ModuleVolkswagen",
+                table: "Posts",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EventDate",
+                schema: "ModuleVolkswagen",
+                table: "Posts",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
