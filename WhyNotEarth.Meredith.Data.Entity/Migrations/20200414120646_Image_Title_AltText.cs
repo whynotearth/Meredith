7b3f6f8b9ef93200a613bc68bf6059a410e97b6f
Kenneth Andersen <andersen.kenneth@gmail.com>
2020-04-14 14:10:49 +0200

    - Added "Title" & "AltText" to "Image" entity

---
+++ WhyNotEarth.Meredith.Data.Entity/Migrations/20200414120646_Image_Title_AltText.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhyNotEarth.Meredith.Data.Entity.Migrations
+{
+    public partial class Image_Title_AltText : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AltText",
+                table: "Images",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Images",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AltText",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Images");
+        }
+    }
+}
