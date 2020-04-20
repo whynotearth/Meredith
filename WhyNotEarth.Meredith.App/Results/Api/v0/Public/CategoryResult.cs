8c2ab699c34386fdd28a240dde8a973aff868511
Shahriar Gholami <shrgholami@gmail.com>
2020-04-20 13:02:22 +0430

    Add slug to category result

--- WhyNotEarth.Meredith.App/Results/Api/v0/Public/CategoryResult.cs
+++ WhyNotEarth.Meredith.App/Results/Api/v0/Public/CategoryResult.cs
@@ -6,15 +6,18 @@ namespace WhyNotEarth.Meredith.App.Results.Api.v0.Public
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Slug { get; set; }
+
+        public string? Name { get; set; }
 
         public string? Image { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public CategoryResult(Category category)
         {
             Id = category.Id;
+            Slug = category.Slug;
             Name = category.Name;
             Image = category.Image?.Url;
             Description = category.Description;
