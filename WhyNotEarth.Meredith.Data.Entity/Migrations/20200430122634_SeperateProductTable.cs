0bf79baa40b18883b26bcf7aef896cfeb98a9240
Shahriar Gholami <shrgholami@gmail.com>
2020-04-30 16:59:46 +0430

    Seperate Shop.Product entity's table

---
+++ WhyNotEarth.Meredith.Data.Entity/Migrations/20200430122634_SeperateProductTable.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WhyNotEarth.Meredith.Data.Entity.Migrations
+{
+    public partial class SeperateProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pages_Prices_PriceId",
+                table: "Pages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderLines_Pages_ProductId",
+                schema: "ModuleShop",
+                table: "OrderLines");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductLocationInventories_Pages_ProductId",
+                schema: "ModuleShop",
+                table: "ProductLocationInventories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Variations_Pages_ProductId",
+                schema: "ModuleShop",
+                table: "Variations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pages_PriceId",
+                table: "Pages");
+
+            migrationBuilder.DropColumn(
+                name: "PriceId",
+                table: "Pages");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Pages");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "ModuleShop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    PageId = table.Column<int>(nullable: false),
+                    PriceId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Pages_PageId",
+                        column: x => x.PageId,
+                        principalTable: "Pages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_Prices_PriceId",
+                        column: x => x.PriceId,
+                        principalSchema: "ModuleShop",
+                        principalTable: "Prices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_PageId",
+                schema: "ModuleShop",
+                table: "Products",
+                column: "PageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_PriceId",
+                schema: "ModuleShop",
+                table: "Products",
+                column: "PriceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderLines_Products_ProductId",
+                schema: "ModuleShop",
+                table: "OrderLines",
+                column: "ProductId",
+                principalSchema: "ModuleShop",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductLocationInventories_Products_ProductId",
+                schema: "ModuleShop",
+                table: "ProductLocationInventories",
+                column: "ProductId",
+                principalSchema: "ModuleShop",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Variations_Products_ProductId",
+                schema: "ModuleShop",
+                table: "Variations",
+                column: "ProductId",
+                principalSchema: "ModuleShop",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderLines_Products_ProductId",
+                schema: "ModuleShop",
+                table: "OrderLines");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductLocationInventories_Products_ProductId",
+                schema: "ModuleShop",
+                table: "ProductLocationInventories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Variations_Products_ProductId",
+                schema: "ModuleShop",
+                table: "Variations");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "ModuleShop");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceId",
+                table: "Pages",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Pages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pages_PriceId",
+                table: "Pages",
+                column: "PriceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pages_Prices_PriceId",
+                table: "Pages",
+                column: "PriceId",
+                principalSchema: "ModuleShop",
+                principalTable: "Prices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderLines_Pages_ProductId",
+                schema: "ModuleShop",
+                table: "OrderLines",
+                column: "ProductId",
+                principalTable: "Pages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductLocationInventories_Pages_ProductId",
+                schema: "ModuleShop",
+                table: "ProductLocationInventories",
+                column: "ProductId",
+                principalTable: "Pages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Variations_Pages_ProductId",
+                schema: "ModuleShop",
+                table: "Variations",
+                column: "ProductId",
+                principalTable: "Pages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- WhyNotEarth.Meredith.Data.Entity/Models/Modules/Shop/Product.cs
+++ WhyNotEarth.Meredith.Data.Entity/Models/Modules/Shop/Product.cs
@@ -1,9 +1,17 @@
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace WhyNotEarth.Meredith.Data.Entity.Models.Modules.Shop
 {
-    public class Product : Page
+    public class Product : IEntityTypeConfiguration<Product>
     {
+        public int Id { get; set; }
+
+        public int PageId { get; set; }
+
+        public Page Page { get; set; }
+
         public int PriceId { get; set; }
 
         public Price Price { get; set; }
@@ -11,5 +19,10 @@ namespace WhyNotEarth.Meredith.Data.Entity.Models.Modules.Shop
         public List<Variation> Variations { get; set; }
 
         public List<ProductLocationInventory> ProductLocationInventories { get; set; }
+
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products", "ModuleShop");
+        }
     }
 }
