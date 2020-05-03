4ab77027edc67fb217c4a432bd629bd2fddc911b
Shahriar Gholami <shrgholami@gmail.com>
2020-05-03 19:05:09 +0430

    Fix some nullability warnings

--- WhyNotEarth.Meredith.App/Models/Api/v0/Reservation/PayModel.cs
+++ WhyNotEarth.Meredith.App/Models/Api/v0/Reservation/PayModel.cs
@@ -4,6 +4,6 @@ namespace WhyNotEarth.Meredith.App.Models.Api.v0.Reservation
 {
     public class PayModel
     {
-        public Dictionary<string, string> Metadata { get; set; }
+        public Dictionary<string, string> Metadata { get; set; } = null!;
     }
 }
--- WhyNotEarth.Meredith/Stripe/Data/StripeCredentialsModel.cs
+++ WhyNotEarth.Meredith/Stripe/Data/StripeCredentialsModel.cs
@@ -1,28 +1,28 @@
+using Newtonsoft.Json;
+
 namespace WhyNotEarth.Meredith.Stripe.Data
 {
-    using Newtonsoft.Json;
-
     public class StripeCredentialsModel
     {
         [JsonProperty("access_token")]
-        public string AccessToken { get; set; }
+        public string AccessToken { get; set; } = null!;
 
         [JsonProperty("livemode")]
         public bool LiveMode { get; set; }
 
         [JsonProperty("refresh_token")]
-        public string RefreshToken { get; set; }
+        public string RefreshToken { get; set; } = null!;
 
         [JsonProperty("scope")]
-        public string Scope { get; set; }
+        public string Scope { get; set; } = null!;
 
         [JsonProperty("stripe_publishable_key")]
-        public string StripePublishableKey { get; set; }
+        public string StripePublishableKey { get; set; } = null!;
 
         [JsonProperty("stripe_user_id")]
-        public string StripeUserId { get; set; }
+        public string StripeUserId { get; set; } = null!;
 
         [JsonProperty("token_type")]
-        public string TokenType { get; set; }
+        public string TokenType { get; set; } = null!;
     }
 }
