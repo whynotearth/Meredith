c787bf7656f695d9816fc4ed9b875d28a4c7c931
Shahriar Gholami <shrgholami@gmail.com>
2020-05-11 16:27:19 +0430

    Add a temporary jumpStartId to the daily plan result

--- WhyNotEarth.Meredith.App/Results/Api/v0/Volkswagen/AdticleDailyResult.cs
+++ WhyNotEarth.Meredith.App/Results/Api/v0/Volkswagen/AdticleDailyResult.cs
@@ -7,12 +7,15 @@ namespace WhyNotEarth.Meredith.App.Results.Api.v0.Volkswagen
 {
     public class ArticleDailyResult
     {
+        public int? JumpStartId { get; set; }
+
         public DateTime Date { get; }
 
         public List<ArticleResult> Articles { get; }
 
         public ArticleDailyResult(DateTime date, List<Article> articles)
         {
+            JumpStartId = articles.FirstOrDefault(item => item.JumpStartId != null)?.JumpStartId;
             Date = date;
             Articles = articles.Select(item => new ArticleResult(item)).ToList();
         }
