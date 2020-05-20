94e67308f7c45d60fc1649a6e42090edc01de4d0
Shahriar Gholami <shrgholami@gmail.com>
2020-05-20 13:40:03 +0430

    Disable VW settings validation

--- WhyNotEarth.Meredith.App/Models/Api/v0/Volkswagen/VolkswagenSettingsModel.cs
+++ WhyNotEarth.Meredith.App/Models/Api/v0/Volkswagen/VolkswagenSettingsModel.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using WhyNotEarth.Meredith.App.Validation;
 
 namespace WhyNotEarth.Meredith.App.Models.Api.v0.Volkswagen
 {
-    public class VolkswagenSettingsModel : IValidatableObject
+    public class VolkswagenSettingsModel
     {
         [Mandatory]
         public bool? EnableAutoSend { get; set; }
@@ -14,25 +12,5 @@ namespace WhyNotEarth.Meredith.App.Models.Api.v0.Volkswagen
         public List<string>? DistributionGroups { get; set; }
 
         public TimeSpan? SendTime { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (EnableAutoSend.HasValue && EnableAutoSend.Value)
-            {
-                if (DistributionGroups is null || !DistributionGroups.Any())
-                {
-                    yield return new ValidationResult(
-                        $"With current value of {nameof(EnableAutoSend)} the {nameof(DistributionGroups)} field is required.",
-                        new[] {nameof(DistributionGroups)});
-                }
-
-                if (SendTime is null)
-                {
-                    yield return new ValidationResult(
-                        $"With current value of {nameof(EnableAutoSend)} the {nameof(SendTime)} field is required.",
-                        new[] {nameof(SendTime)});
-                }
-            }
-        }
     }
 }
