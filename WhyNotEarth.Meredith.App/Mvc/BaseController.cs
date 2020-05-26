c4aea0704c41e1b3d85c93e538228c0515cbb6f9
Shahriar Gholami <shrgholami@gmail.com>
2020-05-26 15:05:22 +0430

    Add a base controler with Created result

---
+++ WhyNotEarth.Meredith.App/Mvc/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhyNotEarth.Meredith.App.Mvc
+{
+    public class BaseController : ControllerBase
+    {
+        [NonAction]
+        public CreateResult Created()
+        {
+            return new CreateResult();
+        }
+    }
+}
---
+++ WhyNotEarth.Meredith.App/Mvc/CreateResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhyNotEarth.Meredith.App.Mvc
+{
+    public class CreateResult : StatusCodeResult
+    {
+        public CreateResult() : base((int) HttpStatusCode.Created)
+        {
+        }
+    }
+}
