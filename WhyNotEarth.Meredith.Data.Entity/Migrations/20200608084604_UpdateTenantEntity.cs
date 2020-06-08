edbafd78df5ff93507d47165a3cf77adbd6658ee
Shahriar Gholami <shrgholami@gmail.com>
2020-06-08 13:17:49 +0430

    Add Tenant updates migration

---
+++ WhyNotEarth.Meredith.Data.Entity/Migrations/20200608084604_UpdateTenantEntity.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WhyNotEarth.Meredith.Data.Entity.Migrations
+{
+    public partial class UpdateTenantEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tenants_Users_UserId",
+                table: "Tenants");
+
+            migrationBuilder.DropTable(
+                name: "Clients",
+                schema: "ModuleShop");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_UserId",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tenants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TenantId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Tenants",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TenantId",
+                table: "Users",
+                column: "TenantId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Tenants_TenantId",
+                table: "Users",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Tenants_TenantId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_TenantId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Tenants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Tenants",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                schema: "ModuleShop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    TenantId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Clients_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_UserId",
+                table: "Tenants",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_TenantId",
+                schema: "ModuleShop",
+                table: "Clients",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_UserId",
+                schema: "ModuleShop",
+                table: "Clients",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tenants_Users_UserId",
+                table: "Tenants",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
