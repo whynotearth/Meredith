3ca86cc778107b801d847935c9cb5e470f7642cb
Shahriar Gholami <shrgholami@gmail.com>
2020-06-17 14:07:01 +0430

    Add PdfUrl column to memo table

---
+++ WhyNotEarth.Meredith.Data.Entity/Migrations/20200617093557_AddPdfUrlToMemo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhyNotEarth.Meredith.Data.Entity.Migrations
+{
+    public partial class AddPdfUrlToMemo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PdfUrl",
+                schema: "ModuleVolkswagen",
+                table: "Memos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PdfUrl",
+                schema: "ModuleVolkswagen",
+                table: "Memos");
+        }
+    }
+}
