90add98c8bd5b94e4d5294ce06700ce0c6859c5c
Shahriar Gholami <shrgholami@gmail.com>
2020-06-18 18:58:36 +0430

    Remove stat fields from Email

---
+++ WhyNotEarth.Meredith.Data.Entity/Migrations/20200618142746_RemoveStatFieldsFromEmail.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhyNotEarth.Meredith.Data.Entity.Migrations
+{
+    public partial class RemoveStatFieldsFromEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClickDateTime",
+                schema: "public",
+                table: "Emails");
+
+            migrationBuilder.DropColumn(
+                name: "DeliverDateTime",
+                schema: "public",
+                table: "Emails");
+
+            migrationBuilder.DropColumn(
+                name: "OpenDateTime",
+                schema: "public",
+                table: "Emails");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ClickDateTime",
+                schema: "public",
+                table: "Emails",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliverDateTime",
+                schema: "public",
+                table: "Emails",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OpenDateTime",
+                schema: "public",
+                table: "Emails",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+    }
+}
