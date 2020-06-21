0f431250acbf2d8dda94f1447becae82b702fa1d
pjanicked <aekkim.13@gmail.com>
2020-06-21 17:50:21 +0530

    Fixed tenant slug creation Fixes #216

--- WhyNotEarth.Meredith/Public/SlugService.cs
+++ WhyNotEarth.Meredith/Public/SlugService.cs
@@ -1,10 +1,30 @@
-﻿namespace WhyNotEarth.Meredith.Public
+﻿using Slugify;
+using System.Threading.Tasks;
+using WhyNotEarth.Meredith.Data.Entity;
+
+namespace WhyNotEarth.Meredith.Public
 {
     public class SlugService
     {
+        private readonly MeredithDbContext _dbContext;
+
+        public SlugService(MeredithDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }        
+
         public string GetSlug(string name)
         {
             return name.ToLower();
+        }
+
+        public async Task<string> UpdateSlug(string name, Data.Entity.Models.Tenant tenant)
+        {
+            tenant.Slug = new SlugHelper().GenerateSlug(name + " " + tenant.Id);
+            _dbContext.Tenants.Update(tenant);
+            await _dbContext.SaveChangesAsync();
+
+            return tenant.Slug;            
         }
     }
 }
