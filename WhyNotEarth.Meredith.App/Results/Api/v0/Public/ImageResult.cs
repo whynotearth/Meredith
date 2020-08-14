7c34eb0d5a139fe132d52320317ce749edcca770
Shahriyar Qulami <ctyar@outlook.com>
2020-08-14 12:23:58 +0430

    Return the new video properties

--- WhyNotEarth.Meredith.App/Results/Api/v0/Public/ImageResult.cs
+++ WhyNotEarth.Meredith.App/Results/Api/v0/Public/ImageResult.cs
@@ -10,11 +10,17 @@ namespace WhyNotEarth.Meredith.App.Results.Api.v0.Public
 
         public int Order { get; }
 
+        public int? Width { get; }
+
+        public int? Height { get; }
+
         public ImageResult(Image image)
         {
             PublicId = image.CloudinaryPublicId;
             Url = image.Url;
             Order = image.Order;
+            Width = image.Width;
+            Height = image.Height;
         }
     }
 }
--- WhyNotEarth.Meredith.App/Results/Api/v0/Public/VideoResult.cs
+++ WhyNotEarth.Meredith.App/Results/Api/v0/Public/VideoResult.cs
@@ -8,10 +8,25 @@ namespace WhyNotEarth.Meredith.App.Results.Api.v0.Public
 
         public string Url { get; }
 
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public double Duration { get; }
+
+        public string Format { get; }
+
+        public string ThumbnailUrl { get; }
+
         public VideoResult(Video video)
         {
             PublicId = video.CloudinaryPublicId;
             Url = video.Url;
+            Width = video.Width;
+            Height = video.Height;
+            Duration = video.Duration;
+            Format = video.Format;
+            ThumbnailUrl = video.ThumbnailUrl;
         }
     }
 }
