92f89c0f263eb3e6a3446dabb7e867775b7639dd
Shahriyar Qulami <ctyar@outlook.com>
2020-10-19 14:19:14 +0330

    Change PDF form widget to accept per page PNG instead of a single PDF url

--- WhyNotEarth.Meredith/BrowTricks/FormWidgets/PdfFormWidget.cs
+++ WhyNotEarth.Meredith/BrowTricks/FormWidgets/PdfFormWidget.cs
@@ -1,73 +1,58 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace WhyNotEarth.Meredith.BrowTricks.FormWidgets
 {
     internal class PdfFormWidget : IFormWidget
     {
         private readonly bool _hasAnswer;
+        private readonly List<string> _urls;
         private readonly string _value;
-        private readonly string _url;
 
         public PdfFormWidget(FormItem formItem)
         {
             _value = formItem.Value;
             _hasAnswer = false;
-            _url = ConvertUrl(formItem.Options?.FirstOrDefault());
+            _urls = formItem.Options ?? new List<string>();
         }
 
         public PdfFormWidget(FormAnswer formAnswer)
         {
             _value = formAnswer.Question;
             _hasAnswer = true;
-            _url = ConvertUrl(formAnswer.Options?.FirstOrDefault());
-        }
-
-        private string ConvertUrl(string? url)
-        {
-            // This seems very iffy
-            // I tried using the Cloudinary library but couldn't figure out how to do it
-            // It was just returning the PDF URL again
-            if (url is null)
-            {
-                return string.Empty;
-            }
-
-            url = url.ToLower();
-
-            var index = url.LastIndexOf(".pdf", StringComparison.Ordinal);
-
-            if (index == -1)
-            {
-                return url;
-            }
-
-            return url.Remove(index, ".pdf".Length).Insert(index, ".png");
+            _urls = formAnswer.Options ?? new List<string>();
         }
 
         public string Render()
         {
-            if (_hasAnswer)
+            var result = new StringBuilder();
+
+            result.AppendLine(
+                "<section class=\"section\">" +
+                $"<p class=\"question mb-2\">{_value}</p>"
+            );
+
+            foreach (var url in _urls)
             {
-                return $@"
-<section class=""section"">
-    <p class=""question mb-2"">{_value}</p>
-    <img class=""mb-2 mt-4"" src=""{_url}"" alt="""" />
-    <p class=""flex items-center"">
-        <img
-            class=""icon-boxtick mr-2""
-            src=""https://res.cloudinary.com/whynotearth/image/upload/v1600070035/BrowTricks/static_backend/boxtick_hxjopo.svg""
-            alt="""" />
-        <span>Accepted</span>
-    </p>
-</section>";
+                result.AppendLine($"<img class=\"mb-2 mt-4\" src=\"{url}\" alt=\"\" />");
             }
 
-            return $@"
-<section class=""section"">
-    <p class=""question mb-2"">{_value}</p>
-    <img class=""mb-2 mt-4"" src=""{_url}"" alt="""" />
-</section>";
+            if (_hasAnswer)
+            {
+                result.AppendLine(
+                    "<p class=\"flex items-center\">" +
+                        "<img" +
+                            "class=\"icon-boxtick mr-2\"" +
+                            "src=\"https://res.cloudinary.com/whynotearth/image/upload/v1600070035/BrowTricks/static_backend/boxtick_hxjopo.svg\"" +
+                            "alt=\"\" />" +
+                        "<span>Accepted</span>" +
+                    "</p>"
+                );
+            }
+
+            result.AppendLine("</section>");
+
+            return result.ToString();
         }
     }
 }
