pjanicked/meredith-core

First pass at a basic set of subscription flows

WhyNotEarth.Meredith/Stripe/StripeCustomerService.cs
@@ -1,0 +1,62 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Stripe;
+using WhyNotEarth.Meredith.Data.Entity;
+using WhyNotEarth.Meredith.Services;
+using WhyNotEarth.Meredith.Stripe.Data;
+
+namespace WhyNotEarth.Meredith.Stripe
+{
+    public class StripeCustomerService : StripeServiceBase, IStripeCustomerService
+    {
+        protected MeredithDbContext MeredithDbContext { get; }
+
+        public StripeCustomerService(IOptions<StripeOptions> stripeOptions,
+            MeredithDbContext meredithDbContext) : base(stripeOptions)
+        {
+            MeredithDbContext = meredithDbContext;
+        }
+
+        public async Task<string> AddCustomerAsync(string? email, string? description)
+        {
+            var customerService = new CustomerService();
+            var customer = await customerService.CreateAsync(new CustomerCreateOptions
+            {
+                Email = email,
+                Description = description
+            }, GetRequestOptions());
+            return customer.Id;
+        }
+
+        public async Task<string> AddCardAsync(string? customerId, string? token)
+        {
+            var cardService = new CardService();
+            var card = await cardService.CreateAsync(customerId, new CardCreateOptions
+            {
+                Source = token
+            }, GetRequestOptions());
+            return card.Id;
+        }
+
+        public async Task<string> AddTokenAsync(string? number, long expirationMonth, long expirationYear)
+        {
+            var tokenService = new TokenService();
+            var token = await tokenService.CreateAsync(new TokenCreateOptions
+            {
+                Card = new CreditCardOptions
+                {
+                    Number = number,
+                    ExpMonth = expirationMonth,
+                    ExpYear = expirationYear
+                }
+            }, GetRequestOptions());
+            return token.Id;
+        }
+
+        public async Task DeleteCardAsync(string? customerId, string? cardId)
+        {
+            var cardService = new CardService();
+            await cardService.DeleteAsync(customerId, cardId, null, GetRequestOptions());
+        }
+    }
+}

WhyNotEarth.Meredith/Stripe/StripeSubscriptionService.cs
@@ -1,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Stripe;
+using WhyNotEarth.Meredith.Data.Entity;
+using WhyNotEarth.Meredith.Services;
+using WhyNotEarth.Meredith.Stripe.Data;
+
+namespace WhyNotEarth.Meredith.Stripe
+{
+    public class StripeSubscriptionService : StripeServiceBase, IStripeSubscriptionService
+    {
+        protected MeredithDbContext MeredithDbContext { get; }
+
+        public StripeSubscriptionService(IOptions<StripeOptions> stripeOptions,
+            MeredithDbContext meredithDbContext) : base(stripeOptions)
+        {
+            MeredithDbContext = meredithDbContext;
+        }
+
+        public async Task<string> AddSubscriptionAsync(string customerId, string planId, string? coupon)
+        {
+            var subscriptionService = new SubscriptionService();
+            var stripeSubscription = await subscriptionService.CreateAsync(new SubscriptionCreateOptions
+            {
+                Customer = customerId,
+                Coupon = coupon,
+                Items = new List<SubscriptionItemOptions>
+                {
+                    new SubscriptionItemOptions
+                    {
+                        Plan = planId
+                    }
+                }
+
+            }, GetRequestOptions());
+            return stripeSubscription.Id;
+        }
+
+        public async Task CancelSubscriptionAsync(string subscriptionId)
+        {
+            var subscriptionService = new SubscriptionService();
+            await subscriptionService.CancelAsync(subscriptionId, null, GetRequestOptions());
+        }
+
+        public async Task ChangeSubscriptionCardAsync(string subscriptionId, string cardId)
+        {
+            var subscriptionService = new SubscriptionService();
+            await subscriptionService.UpdateAsync(subscriptionId, new SubscriptionUpdateOptions
+            {
+                DefaultPaymentMethod = cardId
+            }, GetRequestOptions());
+        }
+
+        public async Task ChangeSubscriptionPlanAsync(string subscriptionId, string planId)
+        {
+            var subscriptionService = new SubscriptionService();
+            var subscriptionItemService = new SubscriptionItemService();
+            var subscription = await subscriptionService.GetAsync(subscriptionId, null, GetRequestOptions());
+            var itemToDeleteId = subscription.Items.First().Id;
+            await subscriptionService.UpdateAsync(subscriptionId, new SubscriptionUpdateOptions
+            {
+                Items = new List<SubscriptionItemOptions>
+                {
+                    new SubscriptionItemOptions
+                    {
+                        Plan = planId
+                    }
+                },
+            }, GetRequestOptions());
+            await subscriptionItemService.DeleteAsync(itemToDeleteId, null, GetRequestOptions());
+        }
+
+        public async Task<string?> GetPlanByName(string name)
+        {
+            var productService = new ProductService();
+            var products = await productService.ListAsync(null, GetRequestOptions());
+            var product = products.FirstOrDefault(p => p.Name == name).Id; ;
+            if (product == null)
+            {
+                return null;
+            }
+
+            var priceService = new PriceService();
+            var prices = await priceService.ListAsync(new PriceListOptions
+            {
+                Product = product
+            }, GetRequestOptions());
+            return prices.FirstOrDefault().Id;
+        }
+    }
+}

whynotearth/Meredith

WhyNotEarth.Meredith/Stripe/StripeCustomerService.cs
@@ -1,0 +1,61 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Stripe;
+using WhyNotEarth.Meredith.Services;
+using WhyNotEarth.Meredith.Stripe.Data;
+
+namespace WhyNotEarth.Meredith.Stripe
+{
+    public class StripeCustomerService : StripeServiceBase, IStripeCustomerService
+    {
+        protected IDbContext MeredithDbContext { get; }
+
+        public StripeCustomerService(IOptions<StripeOptions> stripeOptions,
+            IDbContext meredithDbContext) : base(stripeOptions)
+        {
+            MeredithDbContext = meredithDbContext;
+        }
+
+        public async Task<string> AddCustomerAsync(string? email, string? description)
+        {
+            var customerService = new CustomerService();
+            var customer = await customerService.CreateAsync(new CustomerCreateOptions
+            {
+                Email = email,
+                Description = description
+            }, GetRequestOptions());
+            return customer.Id;
+        }
+
+        public async Task<string> AddCardAsync(string? customerId, string? token)
+        {
+            var cardService = new CardService();
+            var card = await cardService.CreateAsync(customerId, new CardCreateOptions
+            {
+                Source = token
+            }, GetRequestOptions());
+            return card.Id;
+        }
+
+        public async Task<string> AddTokenAsync(string? number, long expirationMonth, long expirationYear)
+        {
+            var tokenService = new TokenService();
+            var token = await tokenService.CreateAsync(new TokenCreateOptions
+            {
+                Card = new TokenCardOptions
+                {
+                    Number = number,
+                    ExpMonth = expirationMonth,
+                    ExpYear = expirationYear
+                }
+            }, GetRequestOptions());
+            return token.Id;
+        }
+
+        public async Task DeleteCardAsync(string? customerId, string? cardId)
+        {
+            var cardService = new CardService();
+            await cardService.DeleteAsync(customerId, cardId, null, GetRequestOptions());
+        }
+    }
+}

WhyNotEarth.Meredith/Stripe/StripeSubscriptionService.cs
@@ -1,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Stripe;
+using WhyNotEarth.Meredith.Services;
+using WhyNotEarth.Meredith.Stripe.Data;
+
+namespace WhyNotEarth.Meredith.Stripe
+{
+    public class StripeSubscriptionService : StripeServiceBase, IStripeSubscriptionService
+    {
+        protected IDbContext MeredithDbContext { get; }
+
+        public StripeSubscriptionService(IOptions<StripeOptions> stripeOptions,
+            IDbContext meredithDbContext) : base(stripeOptions)
+        {
+            MeredithDbContext = meredithDbContext;
+        }
+
+        public async Task<string> AddSubscriptionAsync(string customerId, string planId, string? coupon)
+        {
+            var subscriptionService = new SubscriptionService();
+            var stripeSubscription = await subscriptionService.CreateAsync(new SubscriptionCreateOptions
+            {
+                Customer = customerId,
+                Coupon = coupon,
+                Items = new List<SubscriptionItemOptions>
+                {
+                    new SubscriptionItemOptions
+                    {
+                        Plan = planId
+                    }
+                }
+
+            }, GetRequestOptions());
+            return stripeSubscription.Id;
+        }
+
+        public async Task CancelSubscriptionAsync(string subscriptionId)
+        {
+            var subscriptionService = new SubscriptionService();
+            await subscriptionService.CancelAsync(subscriptionId, null, GetRequestOptions());
+        }
+
+        public async Task ChangeSubscriptionCardAsync(string subscriptionId, string cardId)
+        {
+            var subscriptionService = new SubscriptionService();
+            await subscriptionService.UpdateAsync(subscriptionId, new SubscriptionUpdateOptions
+            {
+                DefaultPaymentMethod = cardId
+            }, GetRequestOptions());
+        }
+
+        public async Task ChangeSubscriptionPlanAsync(string subscriptionId, string planId)
+        {
+            var subscriptionService = new SubscriptionService();
+            var subscriptionItemService = new SubscriptionItemService();
+            var subscription = await subscriptionService.GetAsync(subscriptionId, null, GetRequestOptions());
+            var itemToDeleteId = subscription.Items.First().Id;
+            await subscriptionService.UpdateAsync(subscriptionId, new SubscriptionUpdateOptions
+            {
+                Items = new List<SubscriptionItemOptions>
+                {
+                    new SubscriptionItemOptions
+                    {
+                        Plan = planId
+                    }
+                },
+            }, GetRequestOptions());
+            await subscriptionItemService.DeleteAsync(itemToDeleteId, null, GetRequestOptions());
+        }
+
+        public async Task<string?> GetPlanByName(string name)
+        {
+            var productService = new ProductService();
+            var products = await productService.ListAsync(null, GetRequestOptions());
+            var product = products.FirstOrDefault(p => p.Name == name).Id; ;
+            if (product == null)
+            {
+                return null;
+            }
+
+            var priceService = new PriceService();
+            var prices = await priceService.ListAsync(new PriceListOptions
+            {
+                Product = product
+            }, GetRequestOptions());
+            return prices.FirstOrDefault().Id;
+        }
+    }
+}